1e8010a4e7d483b18dad7ad6d34ddb99538343e9
Fredy Esteban <fredy.esteban25@hotmail.com>
2024-09-27 13:05:43 -0500

    actualizacion

--- Ingenieria/Teoria de la Informacion/Huffman sin interfaz.py
+++ Ingenieria/Teoria de la Informacion/Huffman sin interfaz.py
@@ -34,31 +34,48 @@ class Nodo:
         return f"Nodo(simbolo={self.simbolo}, probabilidad={self.probabilidad})"
 
 def crear_arbol_huffman(probabilidades):
+    aux = []
+    i = 0
     # Crear una cola de prioridad con objetos
-    cola_prioridad = [Nodo(simbolo, probabilidad) for simbolo, probabilidad in probabilidades.items()] #Se hace el diccionario con las letras y la probabilidad
+    cola_prioridad = [Nodo(simbolo, probabilidad*100) for simbolo, probabilidad in probabilidades.items()] #Se hace el diccionario con las letras y la probabilidad
     print("\n Cola antes de heapify:", cola_prioridad)
     heapq.heapify(cola_prioridad) #Organizar de menor a mayor con el constructor __lt__(self, otro):
     print("Cola después de heapify:", cola_prioridad,"\n")
-        
+    
+    numero_datos = len(cola_prioridad)
     # Crear el árbol de Huffman
     while len(cola_prioridad) > 1:
         
         # Extraer los dos objetos con menor probabilidad nodo
         nodo_izquierdo = heapq.heappop(cola_prioridad)
         nodo_derecho = heapq.heappop(cola_prioridad)
+        
+        suma_nodo_izquierdo_derecho = nodo_izquierdo.probabilidad + nodo_derecho.probabilidad
+
+
+        for i in len(cola_prioridad):
+            if cola_prioridad[i].probabilidad == suma_nodo_izquierdo_derecho:
+                suma_nodo_izquierdo_derecho += 1
+
+
+        '''while len(cola_prioridad) != 0:
+            aux.append(heapq.heappop(cola_prioridad))
+            print("vectooor:", aux)
+            i +=1'''
+
         print("nodo izquierdo",nodo_izquierdo)
         print("nodo derecho",nodo_derecho)
         
+
         # Crear un nuevo objeto con la suma de sus probabilidades, sin simbolo
-        nuevo_nodo = Nodo(None, nodo_izquierdo.probabilidad + nodo_derecho.probabilidad)
+        nuevo_nodo = Nodo(None,suma_nodo_izquierdo_derecho )
         print("suma ultimas dos probabilidades",nuevo_nodo,"\n")
         nuevo_nodo.izquierda = nodo_izquierdo
         nuevo_nodo.derecha = nodo_derecho
-                    
-        # Insertar el nuevo objeto en la cola de prioridad
-        heapq.heappush(cola_prioridad, nuevo_nodo)
-        heapq.heapify(cola_prioridad)
-        
+
+        # Insertar el nuevo objeto en la cola de prioridad   
+        heapq.heappush(cola_prioridad, nuevo_nodo) 
+        heapq.heapify(cola_prioridad) 
         print("Cola después de heapify:", cola_prioridad,"\n")
         
     # El último objeto es la raíz del árbol
