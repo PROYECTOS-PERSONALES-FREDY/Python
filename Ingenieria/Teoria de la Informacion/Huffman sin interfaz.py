1c0e71804ab265fac2fdeb01c7e79e445baa1218
FredyxD25 <fredy.esteban25@hotmail.com>
2024-09-27 22:13:05 -0500

    Solucion Bugs

--- Ingenieria/Teoria de la Informacion/Huffman sin interfaz.py
+++ Ingenieria/Teoria de la Informacion/Huffman sin interfaz.py
@@ -1,5 +1,6 @@
 import heapq
-from collections import defaultdict, Counter
+from collections import Counter
+import math
 
 # Paso 1: Calcular la probabilidad de cada letra
 def calcular_probabilidades(frase):
@@ -19,54 +20,47 @@ class Nodo:
     def __init__(self, simbolo, probabilidad):
         self.simbolo = simbolo
         self.probabilidad = probabilidad
-        #self.id = id  # Nuevo atributo para el índice
         self.izquierda = None
         self.derecha = None
     
     def __lt__(self, otro):
-        '''# Primero, comparar por valor; si son iguales, comparar por índice
-        if self.probabilidad  == otro.probabilidad:
-            return self.id > otro.id  # Ordenar más reciente a la derecha'''
         return self.probabilidad < otro.probabilidad #Compara dos objetos, self objeto actual < otro objeto a comparar
     
     # Sobrescribir el método __repr__ para visualizar correctamente
     def __repr__(self):
         return f"Nodo(simbolo={self.simbolo}, probabilidad={self.probabilidad})"
 
+# Paso 3: Calcular la entropia
+def calcular_entropia(probabilidades):
+    entropia = -sum(p * math.log2(p) for p in probabilidades.values())
+    return entropia
+
+
 def crear_arbol_huffman(probabilidades):
-    aux = []
-    i = 0
+
     # Crear una cola de prioridad con objetos
     cola_prioridad = [Nodo(simbolo, probabilidad*100) for simbolo, probabilidad in probabilidades.items()] #Se hace el diccionario con las letras y la probabilidad
     print("\n Cola antes de heapify:", cola_prioridad)
     heapq.heapify(cola_prioridad) #Organizar de menor a mayor con el constructor __lt__(self, otro):
     print("Cola después de heapify:", cola_prioridad,"\n")
     
-    numero_datos = len(cola_prioridad)
     # Crear el árbol de Huffman
     while len(cola_prioridad) > 1:
-        
+        cuenta = 0
         # Extraer los dos objetos con menor probabilidad nodo
         nodo_izquierdo = heapq.heappop(cola_prioridad)
         nodo_derecho = heapq.heappop(cola_prioridad)
         
         suma_nodo_izquierdo_derecho = nodo_izquierdo.probabilidad + nodo_derecho.probabilidad
 
-
-        for i in len(cola_prioridad):
-            if cola_prioridad[i].probabilidad == suma_nodo_izquierdo_derecho:
-                suma_nodo_izquierdo_derecho += 1
-
-
-        '''while len(cola_prioridad) != 0:
-            aux.append(heapq.heappop(cola_prioridad))
-            print("vectooor:", aux)
-            i +=1'''
+        while (cuenta < len(cola_prioridad)):
+            if cola_prioridad[cuenta].probabilidad == suma_nodo_izquierdo_derecho:
+                suma_nodo_izquierdo_derecho -= 1
+            cuenta += 1
 
         print("nodo izquierdo",nodo_izquierdo)
         print("nodo derecho",nodo_derecho)
         
-
         # Crear un nuevo objeto con la suma de sus probabilidades, sin simbolo
         nuevo_nodo = Nodo(None,suma_nodo_izquierdo_derecho )
         print("suma ultimas dos probabilidades",nuevo_nodo,"\n")
@@ -76,6 +70,7 @@ def crear_arbol_huffman(probabilidades):
         # Insertar el nuevo objeto en la cola de prioridad   
         heapq.heappush(cola_prioridad, nuevo_nodo) 
         heapq.heapify(cola_prioridad) 
+        
         print("Cola después de heapify:", cola_prioridad,"\n")
         
     # El último objeto es la raíz del árbol
@@ -89,8 +84,8 @@ def asignar_codigos_huffman(nodo, codigo_actual="", codigos={}):
         print(codigo_actual,codigos)
     else:
         # Recorrer el árbol de forma recursiva
-        asignar_codigos_huffman(nodo.izquierda, codigo_actual + "1", codigos)
         asignar_codigos_huffman(nodo.derecha, codigo_actual + "0", codigos)
+        asignar_codigos_huffman(nodo.izquierda, codigo_actual + "1", codigos)
         
     return codigos
 
@@ -114,11 +109,12 @@ def codificacion_huffman(frase):
 #while(True):
 #frase = input("Introduce una frase: ")
 contador = 0
-frase = ("mi_mama_me_mima")
+frase = ("AAAAABCDDE")
+#frase = ("mi_mama_me_mima")
 
 ''' m   1
-    A   01
-    _   000
+    _   01
+    A   000
     I   0010
     E   0011
 '''
